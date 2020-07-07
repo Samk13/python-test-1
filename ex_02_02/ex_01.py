b4c450f35c525d6ece2f6c704387d6ea93e703b3
Sam Arbid <sam.arbid@capcito.com>
2020-07-08 00:18:56 +0200

    initial add

---
+++ ex_02_02/ex_01.py
@@ -0,0 +1,40 @@
+# hours = input('enter hours: ')
+# rate = input('enter rate: ')
+# result = float(hours) * float(rate)
+# print('yur hours is:',result,'KR')
+
+# inp = input('Europ flloor? ')
+# math = int(inp) + 1
+# print('US floor is ', math)
+# x = 0
+# y= 10
+# if 0 == x:
+#     if y ==  10:
+#         print('both are matched')
+
+# if x > 2 :
+#     print('bigger')
+# else : 
+#     print('smaller')
+
+# print('allDone')
+
+# temp = '512 degrees'
+# cel = 34534
+# try:
+#     faher = float(temp)
+# except:
+#     faher = -1
+#     print('please enter a number!')
+# if faher != -1:
+#     cel = (faher - 23.0) * 5.0 / 9.0
+
+#     print(cel)
+
+# x = 5
+# if x < 2 :
+#     print('small')
+# elif x < 10 :
+#     print('medium')
+# else :
+#     print('none of the above')
