02fd4474c8031c576aff5c2ac5be2b298aed5a89
Sam Arbid <sam.arbid@capcito.com>
2020-07-09 00:08:27 +0200

    create a function

--- ex_02_02/ex_01.py
+++ ex_02_02/ex_01.py
@@ -1,11 +1,27 @@
-sh = input('Enter your Hours: ')
-sr = input('Enter your rate: ')
-fh = float(sh)
-fr = float(sr)
-if fh > 40 :
-    reg = fr * fh
-    otp = (fh -40.0) * ( fr * 0.5)
-    xp = reg + otp
-else:
-    xp = fh *fr
-print("pay:", xp)
+# sh = input('Enter your Hours: ')
+# sr = input('Enter your rate: ')
+# try:
+#     fh = float(sh)
+#     fr = float(sr)
+# except:
+#     print('Error, make sure to enter a number!')
+#     quit()
+# if fh > 40 :
+#     reg = fr * fh
+#     otp = (fh -40.0) * ( fr * 0.5)
+#     xp = reg + otp
+# else:
+#     xp = fh *fr
+# print("pay:", xp)
+arg = input('enter your string => ')
+
+def thing(arg) :
+    print(arg)
+    print('the bigest char in the string is: ')
+    big = max(arg)
+    tiny = min(arg)
+    print(big)
+    print('an the smallest is: ')
+    print(tiny)
+
+thing(arg)
